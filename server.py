d3125291b3700e6b1e2d898092e6d7d300605280
keith <513384683@qq.com>
2017-07-04 10:42:56 +0800

    test

---
+++ server.py
@@ -0,0 +1,15 @@
+
+import web
+
+urls = (
+	'/','index'
+	)
+
+class index:
+	def GET(self):
+		print 1111
+		return '11111'
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
